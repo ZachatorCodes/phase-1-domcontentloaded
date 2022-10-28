08a6597c7497260d56a19488ad08be5741b2e3ff
Zach Schwartz <zachatorcodes@gmail.com>
2022-10-28 14:07:13 -0600

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const text = document.querySelector("#text");
-    text.textContent = "This is really cool!";
+document.addEventListener("DOMContentLoaded", function() { // Creates a new event listener on the document that waits until DOM content loads, then executes the function call.
+    const text = document.querySelector("#text"); // Uses a query selector to find the node with the id "text" and assigns it to a variable named "text".
+    text.textContent = "This is really cool!"; // Changes the text content of the variable to "This is really cool!".
 });
